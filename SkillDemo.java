23efda96886c350fa8532054bc283f736c7de9b8
nbilog <nbilog@ucsd.edu>
2022-05-01 15:59:13 -0700

    created test file

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,4 +1,7 @@
 public class SkillDemo{
-
+    
+    public static int subraction(int a, int b){
+        return a - b;
+    }
 
 }
---
+++ SkillDemoTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester{
+
+    @Test
+    public void subtraction(){
+        assertEquals(2, SkillDemo.subraction(6, 2));
+    }
+}
